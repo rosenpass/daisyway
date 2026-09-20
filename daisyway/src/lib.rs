eb1da1cf185bb175f57105cf17a4bd250771fd1d
Ilka Schulz <ilka@rosenpass.eu>
2026-09-20 11:41:02 +0200

    include contents of `README.md` as documentation of the Rust bins and lib (they did not have any documentation before)

--- daisyway/src/lib.rs
+++ daisyway/src/lib.rs
@@ -1,3 +1,3 @@
+#![doc = include_str!("../../README.md")]
 pub mod internal;
-
 pub use internal::daisyway::{Daisyway, DaisywayConfig};
--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -1,3 +1,5 @@
+#![doc = include_str!("../../README.md")]
+
 use std::{
     net::SocketAddr,
     sync::{
